dbc6684ab0b55eb397ea299163d86af6fd154172
johnnie71 <johnnie.agonz71@gmail.com>
2022-01-12 12:29:15 -0500

    created a function to call the backend to retrieve data

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,18 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-	const [data, setData] = useState([]); // using useState hook to create / edit state
+	const [data, setData] = useState([]); // using useState hook to create / edit state and initializing it as an array
+
+	const callBackendApi = async () => {
+		const response = await fetch("/"); // fetching from the backend get method
+
+		if (response.status !== 200) {
+			// if response is bad it will throw an error!
+			throw Error("Soemthing went wrong");
+		}
+
+		return response;
+	};
 
 	return <div className="App">hey</div>;
 }
